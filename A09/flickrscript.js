4d0fa57568027c40cb5e8e881ca87a03058d480d
Erika Nana <enana@hawaii.edu>
2013-09-10 02:10:03 -1000

    Completed

---
+++ A09/flickrscript.js
@@ -0,0 +1,11 @@
+var b_txt = '';
+
+// write the badge
+
+		 	 		
+	b_txt+= '<a href="http://www.flickr.com/photos/52839779@N02/4920460612/"><img src="http://farm5.staticflickr.com/4096/4920460612_f2c30bf058_s.jpg" alt="A photo on Flickr" title="POGO" height="75" width="75"></a>';
+	
+		 	 		
+	b_txt+= '<a href="http://www.flickr.com/photos/52839779@N02/4920460484/"><img src="http://farm5.staticflickr.com/4095/4920460484_304bfbecd1_s.jpg" alt="A photo on Flickr" title="WebDesign #136 Magazine Cover" height="75" width="75"></a>';
+
+document.write(b_txt);
