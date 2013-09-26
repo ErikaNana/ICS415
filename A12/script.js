0d04da93b138681ac96a0ce636c2676a4ee7da6a
Erika Nana <enana@hawaii.edu>
2013-09-26 03:59:22 -1000

    Complete A12

---
+++ A12/script.js
@@ -0,0 +1,37 @@
+$(document.getElementById('body')).ready(function() {
+	var url;
+	$("#stage").hide();
+ 	$("#one").click(function(){
+	 	url = $("input[name=urlField]").val();
+		//clear display
+		$("#display").empty();
+		$("#display").append("<br>|    Tag     |    Count    |");
+		$('#stage').load(url, function (){
+	
+	    var all = $(this).find("*");
+	    //find each tag
+	    for (i = 0; i < all.length; i++) { // <- here
+	    	var tag = all.get(i).tagName.toLowerCase();
+	    	var tagChildren = $(this).find(tag).length;
+	        var full = "| " + tag + " | " + tagChildren;
+	        		 
+	        var truth = $("#display:contains('" + full + "')").length;
+	        if (truth == 0){
+	        	$("#display").append("<br>" + full); 
+	        }
+	    }
+		});
+    });
+    $("#two").click(function(){
+    	//clear display
+    	$("#display").empty();
+    	url = $("input[name=urlField]").val();
+        $('#stage').load(url, function (){
+        	var scripts = $(this).find("script[type='text/javascript']");
+        	for (i = 0; i < scripts.length; i++){
+        		var script = scripts.get(i).getAttribute("src");
+        		$("#display").append("<br>" + script);
+        	 }        	 
+        });
+    });
+});
