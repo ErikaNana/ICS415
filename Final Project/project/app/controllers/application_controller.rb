101f9c2b2e0606a4b4192c8da651c651b34ec90f
Erika Nana <enana@hawaii.edu>
2013-11-21 14:02:54 -1000

    Fixed no name error

--- Final Project/project/app/controllers/application_controller.rb
+++ Final Project/project/app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-
-  helper_method :current_user
-
-  private
-
-  def current_user
-    session[:current_user]
+    before_filter :set_current_user #requires that a user is logged in for an action to be run
+    protected
+  def set_current_user
+    #raise session[:user_id].inspect
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user != nil
+            raise @current_user.inspect
+    end
+    #redirect_to '/auth/facebook' and return unless @current_user
   end
 end
--- Final Project/project/config/routes.rb
+++ Final Project/project/config/routes.rb
@@ -1,6 +1,6 @@
 Project::Application.routes.draw do
     root :to => "static#home" #controller#action
     get  'auth/facebook/callback' => 'sessions#create',:as => 'login'
-    post 'logout' => 'sessions#destroy'
+    get 'auth/logout' => 'sessions#destroy',  :as => 'logout'
     get  'auth/failure' => 'sessions#failure'
 end
